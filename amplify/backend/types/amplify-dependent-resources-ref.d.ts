eea7d0869afc7173eb7e26422ef5c29c2b20d02b
jackiekosky <jackie@curiositymg.com>
2023-07-26 14:05:23 -0500

    Auth Fixes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,17 +6,6 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIKeyOutput": "string"
     }
   },
-  "auth": {
-    "amplifyappreact0c472650": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "storage": {
     "s30e4c3291": {
       "BucketName": "string",
