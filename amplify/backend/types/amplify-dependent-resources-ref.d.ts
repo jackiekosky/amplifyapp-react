f3ef561c50c9ab9f5df46b89df87f2c11aa42cba
jackiekosky <jackie@curiositymg.com>
2023-07-27 10:21:13 -0500

    Revert "Revert "Testing new Products page""
    This reverts commit 3e526e44e928c2f64bea3252906995e402752d5f.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,7 +6,24 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIKeyOutput": "string"
     }
   },
+  "function": {
+    "amplifyappreact219abec2": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "storage": {
+    "dynamo5b26a23c": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    },
     "s30e4c3291": {
       "BucketName": "string",
       "Region": "string"
