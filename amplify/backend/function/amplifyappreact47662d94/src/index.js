b34fbbe1dd02a088e7b245fd6c58ef4f2fefb8e2
jackiekosky <jackie@curiositymg.com>
2023-07-28 15:48:28 -0500

    Update index.js

--- amplify/backend/function/amplifyappreact47662d94/src/index.js
+++ amplify/backend/function/amplifyappreact47662d94/src/index.js
@@ -4,12 +4,13 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
+    const token = event.queryStringParameters.[id_token];
     return {
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        body: event.pathParameters,
+        body: token,
     };
 };
