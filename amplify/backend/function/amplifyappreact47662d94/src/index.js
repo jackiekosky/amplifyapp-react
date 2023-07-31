42cb322cd3260cdd881ae428f36c3bd1ad761fa7
jackiekosky <101201472+jackiekosky@users.noreply.github.com>
2023-07-30 19:26:49 -0500

    Test

--- amplify/backend/function/amplifyappreact47662d94/src/index.js
+++ amplify/backend/function/amplifyappreact47662d94/src/index.js
@@ -4,14 +4,26 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(event.body);
+    console.log(event.body.url);
+
+    const url = event.body.url;
+    const data = event.body.data;
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
     return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
+        statusCode: res.status,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        body: event.body,
+        body: event.body.url,
     };
 };
