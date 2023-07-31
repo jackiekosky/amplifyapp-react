b41985a733bad0f54ea71855ca6b6544c725f7a5
jackiekosky <101201472+jackiekosky@users.noreply.github.com>
2023-07-30 20:22:33 -0500

    Update index.js

--- amplify/backend/function/amplifyappreact47662d94/src/index.js
+++ amplify/backend/function/amplifyappreact47662d94/src/index.js
@@ -6,7 +6,6 @@
 exports.handler = async (event) => {
     const parsedBody = JSON.parse(event.body); // should wrap in try/catch
     const url = parsedBody.url;
-    const data = JSON.parse(parsedBody.data);
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
@@ -14,6 +13,6 @@ exports.handler = async (event) => {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        body: data,
+        body: url,
     };
 };
