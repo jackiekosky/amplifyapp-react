9d53333c69190fabd604ef1b1380d5fddf16216c
jackiekosky <101201472+jackiekosky@users.noreply.github.com>
2023-07-30 20:09:22 -0500

    Get URL

--- amplify/backend/function/amplifyappreact47662d94/src/index.js
+++ amplify/backend/function/amplifyappreact47662d94/src/index.js
@@ -5,11 +5,10 @@
  */
 exports.handler = async (event) => {
     const parsedBody = JSON.parse(event.body); // should wrap in try/catch
-    const url = parsedBody.url; 
-    const data = parsedBody.data; 
-
+    const url = parsedBody.url; // 'world'
     return {
-        statusCode: res.status,
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
