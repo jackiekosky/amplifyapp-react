1443039424b060ab042f81f70ecb6b10f7b76511
jackiekosky <101201472+jackiekosky@users.noreply.github.com>
2023-07-30 19:57:36 -0500

    Update with proxy test

--- amplify/backend/function/amplifyappreact47662d94/src/index.js
+++ amplify/backend/function/amplifyappreact47662d94/src/index.js
@@ -5,14 +5,26 @@
  */
 exports.handler = async (event) => {
     const parsedBody = JSON.parse(event.body); // should wrap in try/catch
-    const data = parsedBody.hello; // 'world'
+    const url = parsedBody.url; 
+    const data = parsedBody.data; 
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
     return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
+        statusCode: res.status,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        body: data,
+        body: JSON.stringify({
+            access_token: await res.json(),
+        }),
     };
 };
