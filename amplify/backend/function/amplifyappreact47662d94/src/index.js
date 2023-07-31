d9f2066f9c99fd034e4a3d7aa553a9dc61ecdfce
jackiekosky <101201472+jackiekosky@users.noreply.github.com>
2023-07-30 19:47:00 -0500

    Full revert

--- amplify/backend/function/amplifyappreact47662d94/src/index.js
+++ amplify/backend/function/amplifyappreact47662d94/src/index.js
@@ -5,18 +5,9 @@
  */
 exports.handler = async (event) => {
     console.log(event.body);
-/*
-    const res = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-*/
     return {
-        statusCode: res.status,
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
