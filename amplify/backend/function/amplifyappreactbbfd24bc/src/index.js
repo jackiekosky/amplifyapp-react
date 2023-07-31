68e48a0150395d297c2ae9ad1960eae8fa2096ea
jackiekosky <jackie@curiositymg.com>
2023-07-31 14:27:54 -0500

    Update index.js

--- amplify/backend/function/amplifyappreactbbfd24bc/src/index.js
+++ amplify/backend/function/amplifyappreactbbfd24bc/src/index.js
@@ -75,7 +75,6 @@ exports.handler = async (event) => {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        //body: await res.text(),
-        body: JSON.stringify(fetchOptions)
+        body: await res.text()
     };
 };
