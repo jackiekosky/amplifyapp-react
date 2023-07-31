4897c13e44fef1d43edd31f5293d136ab11dda09
jackiekosky <101201472+jackiekosky@users.noreply.github.com>
2023-07-30 20:00:55 -0500

    Update index.js

--- amplify/backend/function/amplifyappreact47662d94/src/index.js
+++ amplify/backend/function/amplifyappreact47662d94/src/index.js
@@ -7,24 +7,12 @@ exports.handler = async (event) => {
     const parsedBody = JSON.parse(event.body); // should wrap in try/catch
     const url = parsedBody.url; 
     const data = parsedBody.data; 
-
-    const res = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-
     return {
         statusCode: res.status,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        body: JSON.stringify({
-            access_token: await res.json(),
-        }),
+        url,
     };
 };
